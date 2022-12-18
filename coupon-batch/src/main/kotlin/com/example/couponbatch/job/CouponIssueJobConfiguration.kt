6885dcbfca80817b1a28efd244e543dc6510deec
jerry.hj <jerry.hj@kakaostyle.com>
2022-12-18 13:25:05 +0900

    chore: input에 따라 특정 job만 trigger

--- coupon-batch/src/main/kotlin/com/example/couponbatch/job/CouponIssueJobConfiguration.kt
+++ coupon-batch/src/main/kotlin/com/example/couponbatch/job/CouponIssueJobConfiguration.kt
@@ -9,12 +9,14 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
 @Configuration
+@ConditionalOnProperty(name = ["spring.batch.job.names"], havingValue = "couponIssueJob")
 class CouponIssueJobConfiguration(
     private val jobBuilderFactory: JobBuilderFactory,
     private val stepBuilderFactory: StepBuilderFactory,
